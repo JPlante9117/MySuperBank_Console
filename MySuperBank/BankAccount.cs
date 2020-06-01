7ec579eebffc1c251f6ef7021ba600536a522b00
Jacques Plante <jacrplante@gmail.com>
2020-06-01 11:51:18 -0400

    created transaction class;

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -10,19 +10,31 @@ namespace MySuperBank
         public string Owner { get; set; }
         private decimal Balance { get; set; }
 
+        private static int accountNumberSeed = 1234567890;
+
         public BankAccount(string name, decimal initialBalance)
         {
             Balance = initialBalance;
             Owner = name;
-            Number = "1023648501";
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
         public void MakeDeposit(decimal depositAmt, DateTime date, string note)
         {
-
+            Balance += depositAmt;
+            Console.WriteLine($"{date} -- NEW BAL {Balance} -- {note}");
         }
         public void MakeWithdrawal(decimal withdrawalAmt, DateTime date, string note)
         {
+            if(Balance - withdrawalAmt < 0)
+            {
+                Console.WriteLine("OVERDRAFT PROTECTION: CANNOT WITHDRAW FUNDS");
+            } else
+            {
+                Balance -= withdrawalAmt;
+                Console.WriteLine($"{date} -- NEW BAL {Balance} -- {note}");
 
+            }
         }
         public decimal GetBalance()
         {
