6659617c7bcc66036aa05d7150bb510c16ca093c
Jacques Plante <jacrplante@gmail.com>
2020-06-01 11:42:48 -0400

    created BankAccount class, implemented in main

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        private decimal Balance { get; set; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Balance = initialBalance;
+            Owner = name;
+            Number = "1023648501";
+        }
+        public void MakeDeposit(decimal depositAmt, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdrawal(decimal withdrawalAmt, DateTime date, string note)
+        {
+
+        }
+        public decimal GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -4,9 +4,24 @@ namespace MySuperBank
 {
     class Program
     {
+        /*
+         * CREATING BANK APP:
+         * 
+         * 1. Accounts have 10 Digit number ID
+         * 2. Has a string that stores the name(s) of the owner(s)
+         * 3. The balance can be retrieved
+         * 4. It accepts deposits
+         * 5. It accepts withdrawals
+         * 6. The initial balance must be positive
+         * 7. Withdrawals cannot make the balance negative
+         * 
+         */
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount("Jacques", 10000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.GetBalance()}");
         }
     }
 }
